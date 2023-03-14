724b97daf8c32d294919d1f2b7b4698393b2e6ab
Dennis Christian Roxas <den4947@gmail.com>
2023-03-14 10:44:45 +0800

    tried configuring host

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname + '/public')));
+app.use('/', express.static(__dirname));
 
 app.use((req, res) => {
     res.status(404);
